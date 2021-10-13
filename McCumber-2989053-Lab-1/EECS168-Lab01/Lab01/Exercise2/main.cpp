5fbf7f81b9cea44ecab3bff776cef2164c717259
mccumberluke <84547253+mccumberluke@users.noreply.github.com>
2021-10-12 22:22:46 -0500

    Add files via upload

---
+++ McCumber-2989053-Lab-1/EECS168-Lab01/Lab01/Exercise2/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+int main(){
+  std::cout << "My name is Luke McCumber.\n";
+  std::cout << "I am an EECS major.\n";
+  std::cout << "My hobbies are:\n";
+  std::cout << "\tvideo games\n";
+  std::cout << "\twatching sports\n";
+  std::cout << "\thanging out with firends\n";
+  std::cout << "\texercising\n";
+  std::cout << "Goodbye\n";
+  return(0);
+}
